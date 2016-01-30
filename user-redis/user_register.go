2a4be8c675164051260120c593914e8aebf74ea2
Arvind Kumar C <arvindkumar.c@gmail.com>
2016-01-30 16:27:04 +0530

    Pulled out POST as separate method. saved into redis.

--- user-redis/user_register.go
+++ user-redis/user_register.go
@@ -16,24 +16,18 @@ type User struct {
 
 func main() {
 	http.HandleFunc("/sign_up", func(w http.ResponseWriter, r *http.Request) {
-		decoder := json.NewDecoder(r.Body)
-    	var user User
 
-    	err := decoder.Decode(&user)
-    	if err != nil {
-    		fmt.Println("error parsing request..", err)
-    		fmt.Fprintf(w, "Failed to parse request")
-    		return
-    	}
+		if r.Method == "POST" {
+			err := saveUser(w, r)
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+				return
+			}
 
-    	err = saveUser(user)
-    	if err != nil {
-    		fmt.Fprintf(w, "Faild to connect to redis")
-    		return
-    	}
+			fmt.Fprintf(w, "<b>User registered!</b>")
+		} else if r.Method == "GET" {
 
-    	fmt.Println("Received: ", user)
-		fmt.Fprintf(w, "<b>User " + user.Name + " registered!</b>")
+		}
 	})
 
 	fmt.Println("Listening to :", os.Getenv("HTTP_PORT"))
@@ -43,11 +37,28 @@ func main() {
 	}
 }
 
-func saveUser(user User) error {
+func saveUser(w http.ResponseWriter, r *http.Request) error {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Println("error parsing request..", err)
+		fmt.Fprintf(w, "Failed to parse request")
+		return err
+	}
+
 	redisURL := os.Getenv("REDIS_URL")
-	_, err := redis.Dial("tcp", redisURL)
+	client, err := redis.Dial("tcp", redisURL)
 	if err != nil {
 		fmt.Println("error connecting to redis." + redisURL + ". " + err.Error())
+		return err
+	}
+
+	user_json, _ := json.Marshal(user)
+	err = client.Cmd("SET", user.Name, user_json).Err
+	if err != nil {
+		fmt.Println("unable to save to redis" + err.Error())
 		return err
 	}
 
