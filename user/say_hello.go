6cb3557cbc9f43cfc2bdb447a6819b3dc0d72f99
Arvind Kumar C <arvindkumar.c@gmail.com>
2016-01-30 13:41:15 +0530

    Added git as a dependency so that go get can be done

--- user/say_hello.go
+++ user/say_hello.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<b>Hello!</b>")
 	})
-	fmt.Println("HTTP_PORT:", os.Getenv("HTTP_PORT"))
-	http.ListenAndServe(":" + os.Getenv("HTTP_PORT"), nil)
+
+	fmt.Println("Listening to :", os.Getenv("HTTP_PORT"))
+	err := http.ListenAndServe(":" + os.Getenv("HTTP_PORT"), handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
+	if err != nil {
+		fmt.Println("Error starting server.", err)
+	}
 }
